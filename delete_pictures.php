26038294cfbe991752c954ddfd78df646d825d1e
Luca <100572731+IzeLeam@users.noreply.github.com>
2025-12-10 13:54:12 +0000

    Revert "test"
    This reverts commit 942a2fb44b9aa39b9e18b13c3cd211412d6d0302.

--- delete_pictures.php
+++ delete_pictures.php
@@ -16,27 +16,19 @@ else{
 
         $pictures_name = $_POST['pictures_name'];
 
-        $verify_sql = $connection->prepare("SELECT pictures_name FROM pictures INNER JOIN users ON pictures.id_users = users.users_id WHERE pictures_name = ? AND users.users_username = ?");
-        $verify_sql->bind_param("ss", $pictures_name, $_SESSION['username']);
-        $verify_sql->execute();
-        $verify_result = $verify_sql->get_result();
+        $sql2 = $connection->prepare("DELETE FROM pictures WHERE pictures_name = ?");
+        $sql2->bind_param("s", $pictures_name);
 
-        if ($verify_result->num_rows > 0) {
-            $sql2 = $connection->prepare("DELETE FROM pictures WHERE pictures_name = ?");
-            $sql2->bind_param("s", $pictures_name);
+        if ($sql2->execute()) {
 
-            if ($sql2->execute()) {
+            $path = "uploads/" . basename($pictures_name);
 
-                $path = "uploads/" . basename($pictures_name);
-
-                if (file_exists($path) && unlink($path)) {
-                    echo "Removed picture " . htmlspecialchars($path) . "<br>";
-                    echo "Removed picture " . htmlspecialchars($pictures_name) . ", continue with <a href=''>deleting pictures</a>";
-                }
+            if (unlink($path)) {
+                echo "Removed picture " . $path . "<br>";
+                echo "Removed picture " . $pictures_name . ", continue with <a href=''>deleting pictures</a>";
             }
-            $sql2->close();
         }
-        $verify_sql->close();
+        $sql2->close();
     }
     
     $sql1 = "SELECT users.users_username, pictures.pictures_name FROM pictures INNER JOIN users ON pictures.id_users = users.users_id";
