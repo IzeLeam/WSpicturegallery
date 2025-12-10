6c1ee81c2b143c0ce0a8b8b5699dfb9f9b786ca8
Luca <100572731+IzeLeam@users.noreply.github.com>
2025-12-10 10:41:46 +0000

    fix

--- delete_pictures.php
+++ delete_pictures.php
@@ -14,21 +14,30 @@ if (!isset ($_SESSION ['username'])){
 else{
     if (isset($_POST['pictures_name'])) {
 
-        $pictures_name = $_POST['pictures_name'];
+        $pictures_name = basename($_POST['pictures_name']);
 
-        $sql2 = $connection->prepare("DELETE FROM pictures WHERE pictures_name = ?");
-        $sql2->bind_param("s", $pictures_name);
+        // On vérifie que l'utilisateur possède bien cette image
+        $sql_check = $connection->prepare("SELECT pictures_name FROM pictures INNER JOIN users ON pictures.id_users = users.users_id WHERE pictures_name = ? AND users.users_username = ?");
+        $sql_check->bind_param("ss", $pictures_name, $_SESSION['username']);
+        $sql_check->execute();
+        $result_check = $sql_check->get_result();
 
-        if ($sql2->execute()) {
+        if ($result_check->num_rows > 0) {
+            // L'utilisateur possède l'image, on peut supprimer
+            $sql2 = $connection->prepare("DELETE FROM pictures WHERE pictures_name = ?");
+            $sql2->bind_param("s", $pictures_name);
 
-            $path = "uploads/" . basename($pictures_name);
+            if ($sql2->execute()) {
+                $path = "uploads/" . $pictures_name;
 
-            if (unlink($path)) {
-                echo "Removed picture " . $path . "<br>";
-                echo "Removed picture " . $pictures_name . ", continue with <a href=''>deleting pictures</a>";
+                if (unlink($path)) {
+                    echo "Removed picture " . $path . "<br>";
+                    echo "Removed picture " . $pictures_name . ", continue with <a href=''>deleting pictures</a>";
+                }
             }
+            $sql2->close();
         }
-        $sql2->close();
+        $sql_check->close();
     }
     
     $sql1 = "SELECT users.users_username, pictures.pictures_name FROM pictures INNER JOIN users ON pictures.id_users = users.users_id";
